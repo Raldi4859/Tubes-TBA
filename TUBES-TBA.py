3945206b144cd35e28dbb40062dcec33d66645bb
Rasyid Riyaldi <64974343+Raldi4859@users.noreply.github.com>
2022-06-08 16:18:01 +0700

    Update TUBES-TBA.py

--- TUBES-TBA.py
+++ TUBES-TBA.py
@@ -150,6 +150,7 @@ def lexical(sentence, check):
 
 #--------------------------------------------------MAIN PROGRAM---------------------------------------------------------------------------------
 check = False
+count = 0
 st.write('\n  TUGAS BESAR TEORI BAHASA DAN AUTOMATA | KELOMPOK 15 | IF-44-01')
 
 while check != True:
@@ -161,7 +162,8 @@ while check != True:
   st.write('|| VB  : eat, bring, play, read                                ||')
   st.write('|| OB  : pancake, bottle, piano, book                          ||')
   st.write('=================================================================')
-  sentence = st.text_input('Input                          : ', key = 1)
+  sentence = st.text_input('Input                          : ', key = count)
+  count += 1
   
   input_string = sentence.lower() + '#'
   check = lexical(sentence, check)
